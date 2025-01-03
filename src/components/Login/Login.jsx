2bfa4ccbdd73fc6cae9ed39ed2f81d53e640ebee
Your Name <41466655+Sajaalghoul@users.noreply.github.com>
2025-01-03 08:53:24 +0200

    login page styled

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,30 @@
-import React from "react";
 import LoginNav from "../LoginNav/LoginNav";
+import styles from "./Login.module.css";
 
 const Login = () => {
   return (
     <>
       <LoginNav />
-      <div></div>
+      <div className={styles.LoginHero}>
+        <div className={styles.LoginLeftSide}>
+          <h1 className={styles.LoginTitle}>
+            Welcome To Your Professioanl Community
+          </h1>
+          <button className={styles.GoogleLoginButton}>
+            <img
+              src="../../assets/images/google_logo_icon.webp"
+              alt="Google Icon"
+              className={styles.GoogleIcon}
+            />
+            <p>Continue With Google</p>
+          </button>
+        </div>
+        <img
+          src="../../assets/images/login-hero.png"
+          alt="HeroImage"
+          className={styles.HeroImage}
+        />
+      </div>
     </>
   );
 };
--- src/components/LoginNav/LoginNav.jsx
+++ src/components/LoginNav/LoginNav.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import styles from './LoginNav.module.css';
+import styles from "./LoginNav.module.css";
 
 const LoginNav = () => {
   return (
